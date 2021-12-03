79d90cebb706a4f048fb36e3f5033cf823e42111
mmcelwee8002 <mmcelwee8002@gmail.com>
2021-12-02 20:21:25 -0600

    learning js.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,36 @@
+
 window.alert("this is an alert! JavaScript is running!");
 
+function fight() {window.alert ("The fight has begun!");}
+
+
+
+
+var playerName =  window.prompt("What is your robot's name?");
+
+
+// what is this?
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+//thiswill do math and log 20
+console.log(10+10);
+//what is this?
+console.log("our robot's name is " + playerName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fight();
